0c9c3f1400a1df24fb6938e42c89b80a8e69b50b
nur-magomed <nur-magomed@mail.ru>
2023-07-30 17:50:34 +0300

    homework 05 init

---
+++ homework-05/src/test/java/edu/nur/homework05/Homework05ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nur.homework05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
